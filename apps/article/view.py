a812231bed1f356e6dba6cf9648732e96f3ed45b
lkw <1005446851@qq.com>
2020-07-25 09:46:18 +0800

    修改数据库，添加add_article.html

--- apps/article/view.py
+++ apps/article/view.py
@@ -1,3 +1,14 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request
+
+from apps.user.models import User
 
 article_bp = Blueprint('article', __name__)
+
+
+@article_bp.route('/publish', methods=['GET', 'POST'])
+def publish_article():
+    if request.method == 'POST':
+        pass
+    else:
+        users = User.query.filter(User.isdelete == False).all()
+        return render_template('‘article/add_article.html)', users=users)
